15f612f9b393ef38bc725f480774466ed26ccca7
syamsiahnur-gif <syamsiahasyanur@gmail.com>
2026-09-18 07:13:50 +0700

    Update 01_total_pendapatan_per_bulan.sql

--- queries/01_total_pendapatan_per_bulan.sql
+++ queries/01_total_pendapatan_per_bulan.sql
@@ -2,7 +2,7 @@
 -- Pendapatan dihitung dari KWh_Terpakai x Tarif_Per_KWh (Dimensi_Layanan)
 
 SELECT 
-    DATE_TRUNC('month', t.Tanggal_Waktu) AS bulan,
+    DATE_TRUNC('month', t.Tanggal_Waktu) :: DATE AS bulan,
     SUM(t.KWh_Terpakai * l.Tarif_Per_KWh) AS total_pendapatan
 FROM Data_Transaksi t
 JOIN Dimensi_Layanan l ON t.ID_Layanan = l.ID_Layanan
